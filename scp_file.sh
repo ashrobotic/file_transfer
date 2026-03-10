607c4f271c2e612d464b1ab074fffc0216e3722a
ashwini <ashwinib2103@gmail.com>
2026-03-10 12:16:07 +0530

    Added automatic SCP file transfer script (Ubuntu -> Windows)

---
+++ scp_file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WATCH_DIR="$HOME/patient_records"
+WINDOWS_USER="robot"
+WINDOWS_IP="192.168.0.175"
+WINDOWS_PATH="/home/robot/Documents"
+WIN_PASS="Ninos@2025"
+
+echo "Monitoring $WATCH_DIR for new txt files..."
+
+inotifywait -m -e close_write --format '%f' "$WATCH_DIR" | while IFS= read -r FILE
+do
+    if [[ "$FILE" == *.txt ]]; then
+        echo "New file detected: $FILE"
+
+        sshpass -p "$WIN_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$WATCH_DIR/$FILE" ${WINDOWS_USER}@${WINDOWS_IP}:"${WINDOWS_PATH}"
+
+        if [ $? -eq 0 ]; then
+            echo "File transferred successfully"
+        else
+            echo "Transfer failed"
+        fi
+    fi
+done
